dbfc6adbb18a92e3a8ea416f1d803a81d4b14437
adamwoolhether <adamwoolhether@gmail.com>
2023-02-04 00:02:32 +0800

    basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::fmt;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
+
+use warp::Filter;
 
 #[derive(Debug)]
 struct Question {
@@ -13,8 +14,8 @@ struct Question {
 struct QuestionId(String);
 
 impl Question {
-    fn new (id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
-        Question{
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
             id,
             title,
             content,
@@ -29,21 +30,14 @@ impl FromStr for QuestionId {
     fn from_str(id: &str) -> Result<Self, Self::Err> {
         match id.is_empty() {
             false => Ok(QuestionId(id.to_string())),
-            true => Err (
-                Error::new(ErrorKind::InvalidInput, "No id provided")
-                )
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
         }
     }
 }
 
-fn main() {
-  let question = Question::new(
-      // Don't use unwrap or expect in production. Handle errors with `match`, unless you
-      // catch errors and handle the gracefull.
-      QuestionId::from_str("1").expect("No id provided"),
-      "First Question".to_string(),
-      "Content of question".to_string(),
-      Some(vec!("faq".to_string())),
-  );
-    println!("{:?}", question);
+#[tokio::main]
+async fn main() {
+    let hello = warp::get().map(|| format!("Hello, World!"));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
