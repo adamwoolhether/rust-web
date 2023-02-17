d1d4588b438f80124832282f12660afb6495590b
adamwoolhether <adamwoolhether@gmail.com>
2023-02-18 00:25:49 +0800

    LOG_TRACE_DEBUG: Use log4rs package for better log handling

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ mod types;
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    // env_logger::init();
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
 
     log::error!("This is an error");
     log::info!("This is info");
